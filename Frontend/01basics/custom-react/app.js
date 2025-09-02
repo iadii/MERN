41873c69ae742cce97090fb812414fb21064551f
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-03 03:00:06 +0530

    custom react

---
+++ Frontend/01basics/custom-react/app.js
@@ -0,0 +1,33 @@
+
+
+function customRender(reactElement, rootContainer){
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children; 
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    rootContainer.appendChild(domElement) 
+} 
+
+// with better syntax 
+function customRendering(reactElememnt, rootContainer){
+    const domElement = document.createElement(reactElememnt.type)
+    domElement.innerHTML = reactElememnt.children;
+
+    for (const prop in reactElememnt.props) {
+        domElement.setAttribute(prop, reactElememnt.props[prop])
+    }
+    rootContainer.appendChild(domElement)
+}
+
+const root = document.querySelector('#root')
+const reactElement = {
+    type: "a",
+    props: {
+        href: "google.com",
+        target: "_blank"
+    },
+    children: 'click me to visit google'
+}  
+
+customRender(reactElement, root)
+customRendering(reactElement, root)
