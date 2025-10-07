0501658041ce93f90e942438416cceb6f0165234
iadii <mishraaditya3456@gmail.com>
2025-10-07 18:55:20 +0530

    practising

---
+++ Backend/Day01/Simple-express-server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.status.json({
+        msg: "Hii Adii"
+    })
+})
+
+app.get('/adii', (req, res) => {
+    res.json({
+        adii: "vats"
+    })
+})
