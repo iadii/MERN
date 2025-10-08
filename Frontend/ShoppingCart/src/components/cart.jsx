0ddfd496402c2dbfd9c8badb9c168646f6c17c3e
iadii <mishraaditya3456@gmail.com>
2025-10-08 20:58:52 +0530

    fetching backend data

--- Frontend/ShoppingCart/src/components/cart.jsx
+++ Frontend/ShoppingCart/src/components/cart.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function Cart() {
   const [cart, setCart] = useState([]);
@@ -12,20 +12,16 @@ export function Cart() {
   return (
     <div>
       <h2>Shopping Cart</h2>
-      {cart.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
         <div>
           {cart.map((item) => (
             <div key={item.id}>
               <img src={item.image} alt={item.item} width="100" />
               <h3>{item.item}</h3>
-              <p>Price: ${item.price}</p>
+              <p>Price: ₹{item.price}</p>
               <p>Category: {item.category}</p>
             </div>
           ))}
         </div>
-      )}
     </div>
   );
 }
