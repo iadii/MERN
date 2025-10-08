8de74a144d9e409dbb8157266d02896089f67e30
iadii <mishraaditya3456@gmail.com>
2025-10-09 02:43:04 +0530

    you can find its backend in BAckend/Day03/shoppingCart-backend

---
+++ Frontend/Day04/ShoppingCart/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Cart } from './components/cart'
+
+function App() {
+
+  return (
+    <>
+      <Cart/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/Day04/ShoppingCart/src/components/cart.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export function Cart() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/cart").then(async (res) => {
+      const data = await res.json();
+      setCart(data);
+    });
+  });
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+        <div>
+          {cart.map((item) => (
+            <div key={item.id}>
+              <img src={item.image} alt={item.item} width="300" />
+              <h3>{item.item}</h3>
+              <p>Price: ₹{item.price}</p>
+              <p>Category: {item.category}</p>
+            </div>
+          ))}
+        </div>
+    </div>
+  );
+}
