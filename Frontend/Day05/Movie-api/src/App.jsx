4e7629e0a60b84e64919e3908d4321937f59a9aa
iadii <mishraaditya3456@gmail.com>
2025-10-10 03:05:25 +0530

    fetch movie

---
+++ Frontend/Day05/Movie-api/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Movie } from "./components/Movie"
+import { Wrap } from "./components/Wrap"
+
+function App() {
+
+  return (
+    <>
+        <Movie />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/Day05/Movie-api/src/components/Wrap.jsx
@@ -0,0 +1,7 @@
+export function Wrap({ children }){
+    return (
+        <>
+            {children}
+        </>
+    )
+}
