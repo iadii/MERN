5a43bf3d0485e8d932ca7c845f9a7c359d59977c
iadii <mishraaditya3456@gmail.com>
2025-10-25 23:16:22 +0530

    async handler uti

--- Backend/Project-camp/src/controllers/healthCheck.controller.js
+++ Backend/Project-camp/src/controllers/healthCheck.controller.js
@@ -1,11 +1,22 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const healthCheck = (req, res) => {
+//since we have created asunc handler util no  need of write try-catch
+/* const healthCheck = async (req, res, next) => {
   try {
     res
       .status(200)
       .json(new ApiResponse(200, { message: "Server is runnig " }));
-  } catch (error) {}
-};
+  } catch (error) {
+    next(error);
+  }
+}; */
 
-export { healthCheck }
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+  .status(200)
+  .json(new ApiResponse(200, { message: "Server is runnig " }));
+  
+});
+
+export { healthCheck };
--- Backend/Project-camp/src/routes/healthCheck.routes.js
+++ Backend/Project-camp/src/routes/healthCheck.routes.js
@@ -3,7 +3,7 @@ import { healthCheck } from "../controllers/healthCheck.controller.js";
 
 const router = Router()
 
-// router.route("/").get(healthCheck)
+router.route("/").get(healthCheck)
 // router.get("/",healthCheck) -> this will also work
-router.get("/",healthCheck)
+
 export default router;
