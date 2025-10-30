d0de2e6e0f6d07ee53cfe3381befa06c6dac0be4
iadii <mishraaditya3456@gmail.com>
2025-10-30 09:28:40 +0530

    hashing

--- Backend/Project-camp/src/models/user.models.js
+++ Backend/Project-camp/src/models/user.models.js
@@ -61,9 +61,14 @@ const UserSchema = new Schema({
 );
 
 
-UserSchema.pre("save", async (next) => {
+UserSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next()
     this.password = await bcrypt.hash(this.password, 10)
     next
 })
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
 mongoose.model("User", UserSchema);
