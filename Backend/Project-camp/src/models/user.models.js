498ac26023d12c21cabf07521ee0b66c93a2fc0d
iadii <mishraaditya3456@gmail.com>
2026-02-17 05:49:02 +0530

    feat: added access and refresh token

--- Backend/Project-camp/src/models/user.models.js
+++ Backend/Project-camp/src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
 
 const UserSchema = new Schema({
     avatar: {
@@ -34,7 +35,7 @@ const UserSchema = new Schema({
     },
     password: {
         type: String,
-        required: [true, "Password is required"] 
+        required: [true, "Password is required"]
     },
     isEmailVerified: {
         type: Boolean,
@@ -55,14 +56,14 @@ const UserSchema = new Schema({
     emailVerficationExpiry: {
         type: Date
     }
-},{
+}, {
     timestamps: true
 }
 );
 
 
 UserSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next()
+    if (!this.isModified("password")) return next()
     this.password = await bcrypt.hash(this.password, 10)
     next
 })
@@ -71,6 +72,29 @@ UserSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+UserSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            //this is more than enough for payload
+            // we can access everything in db using _id(given by mognodb)
+            _id: this._id,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            username: this.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
 
 mongoose.model("User", UserSchema);
 
