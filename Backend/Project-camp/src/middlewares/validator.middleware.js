4ae16e8d84a0af20b68611ce6e4ca935313bba44
iadii <mishraaditya3456@gmail.com>
2026-02-18 23:16:02 +0530

    feat: added express validator

--- Backend/Project-camp/src/middlewares/validator.middleware.js
+++ Backend/Project-camp/src/middlewares/validator.middleware.js
@@ -1,15 +1,12 @@
 import { validationResult } from "express-validator";
 import { ApiError } from "../utils/api-error.js";
 
-
-
-
 export const validate = (req, res, next) => {
     const errors = validationResult(req)
-    if(errors.isEmpty()){
+    if (errors.isEmpty()) {
         return next()
     }
     const exctractedErrors = []
-    errors.array().map((err) => exctractedErrors.push({[err.path]: err.msg}))
+    errors.array().map((err) => exctractedErrors.push({ [err.path]: err.msg }))
     throw new ApiError(422, "Recieved data is not valid ", exctractedErrors)
 }
