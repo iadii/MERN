ca290bea96e3c0cf7caa19fecfce6443addb8d32
iadii <mishraaditya3456@gmail.com>
2026-02-18 22:47:52 +0530

    feat: added express validator

---
+++ Backend/Project-camp/src/middlewares/validator.middleware.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error.js";
+
+
+
+
+export const validate = (req, res, next) => {
+    const errors = validationResult(req)
+    if(errors.isEmpty()){
+        return next()
+    }
+    const exctractedErrors = []
+    errors.array().map((err) => exctractedErrors.push({[err.path]: err.msg}))
+}
