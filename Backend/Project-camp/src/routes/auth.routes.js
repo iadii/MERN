cb6352cee5710d6acfdaa118ae49d0d7bf6e41f8
iadii <mishraaditya3456@gmail.com>
2026-02-18 23:12:45 +0530

    feat: added custom validator

--- Backend/Project-camp/src/routes/auth.routes.js
+++ Backend/Project-camp/src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express"
 import { registerUser } from "../controllers/auth.controller.js"
+import {validate} from "../middlewares/validator.middleware.js"
+
 
 const router = Router();
 
