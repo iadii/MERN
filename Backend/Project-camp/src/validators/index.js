f541922889e5379a24d4cbab197ea76c56b6baa1
iadii <mishraaditya3456@gmail.com>
2026-02-18 23:09:04 +0530

    feat: added custom validator

--- Backend/Project-camp/src/validators/index.js
+++ Backend/Project-camp/src/validators/index.js
@@ -9,14 +9,25 @@ const userRegisterValidator = () => {
             .isEmail()
             .withMessage("Email is invalid"),
 
-            body("username")
-                .trim()
-                .notEmpty()
-                .withMessage("Username is required")
-                .isLowercase()
-                .withMessage("Username must be in lowercase")
-                .isLength({min: 3})
-                .withMessage("Username must be atleast 3 characters")
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is required")
+            .isLowercase()
+            .withMessage("Username must be in lowercase")
+            .isLength({min: 3})
+            .withMessage("Username must be atleast 3 characters"),
+
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required")
+            .isLength({min: 8})
+            .withMessage("Password must be atleast 8 character"),
+        
+        body("fullname")
+            .optional()
+            .trim()                
 
     ]
 }
