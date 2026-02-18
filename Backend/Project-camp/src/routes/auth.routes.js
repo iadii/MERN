3f064e984bcadb44bfd52a64c6b1fb7d7e5ce413
iadii <mishraaditya3456@gmail.com>
2026-02-19 01:47:23 +0530

    feat:created login controller

--- Backend/Project-camp/src/routes/auth.routes.js
+++ Backend/Project-camp/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { login, registerUser } from "../controllers/auth.controller.js"
-import {validate} from "../middlewares/validator.middleware.js"
+import { validate } from "../middlewares/validator.middleware.js"
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js";
 
 
@@ -9,6 +9,5 @@ const router = Router();
 router.route('/register').post(userRegisterValidator(), validate, registerUser)
 
 router.route('/login').post(userLoginValidator(), validate, login)
-// router.route('login').post(login)
 
 export default router;
