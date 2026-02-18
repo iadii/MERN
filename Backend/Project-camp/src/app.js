f1adf803a540ba96f77133ff587be03953dd15cb
iadii <mishraaditya3456@gmail.com>
2026-02-18 23:40:34 +0530

    feat: added cookie parser

--- Backend/Project-camp/src/app.js
+++ Backend/Project-camp/src/app.js
@@ -3,9 +3,11 @@ import cors from 'cors'
 
 import healthCheck from './routes/healthCheck.routes.js';
 import authRouter from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cookieParser())
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
