3dc14444ebfb6e26d3f4942ffe3f2f05182b7d13
iadii <mishraaditya3456@gmail.com>
2026-02-21 18:33:16 +0530

    feat: added current USer routes

---
+++ Backend/Project-camp/src/controllers/currentUser.controller.js
@@ -0,0 +1,17 @@
+import { User } from '../models/user.models.js'
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200, req.user,
+                    "Current user fetched successfully"
+                )
+            )
+})
+
+export { getCurrentUser }
--- Backend/Project-camp/src/routes/auth.routes.js
+++ Backend/Project-camp/src/routes/auth.routes.js
@@ -5,6 +5,7 @@ import { userRegisterValidator, userLoginValidator } from "../validators/index.j
 import { logoutUser } from "../controllers/logout.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { login } from "../controllers/login.controller.js";
+import { getCurrentUser } from "../controllers/currentUser.controller.js";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.route('/register').post(userRegisterValidator(), validate, registerUser)
 
 router.route('/login').post(userLoginValidator(), validate, login)
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/current-user').post(getCurrentUser)
 
 export default router;
