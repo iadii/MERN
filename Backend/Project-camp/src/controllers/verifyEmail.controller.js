35336452d19184a354597a724007a6db1b598d73
iadii <mishraaditya3456@gmail.com>
2026-02-21 19:02:22 +0530

    email verification controller

--- Backend/Project-camp/src/controllers/verifyEmail.controller.js
+++ Backend/Project-camp/src/controllers/verifyEmail.controller.js
@@ -3,12 +3,31 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const verifyEmail = asyncHandler(async(req, res) => {
-    const {verificationToken} = req.params
+const verifyEmail = asyncHandler(async (req, res) => {
+    const { verificationToken } = req.params
 
-    if(!verificationToken){
+    if (!verificationToken) {
         throw new ApiError(400, "Email verification token is missing")
     }
+
+    let hashedToken = crypto
+        .createHash("256")
+        .update(verificationToken)
+        .digest("hex")
+
+    const user = await User.findOne({
+        emailVerificationToken: hashedToken,
+        emailVerificationExpiry: { $gt: Date.now() }
+    })
+    if (!user) {
+        throw new ApiError(400, "Email verification code is invalid")
+    }
+    // cleaning up unnecessary data which is in user model
+    user.emailVerificationToken = undefined
+    user.emailVerificationExpiry = undefined
+    
+    user.isEmailVerified = true
+    await user.save({ validateBeforeSave: false })
 })
 
 export { verifyEmail }
