86debf23baca2750bb227bdebd86714ba1555a52
iadii <mishraaditya3456@gmail.com>
2026-02-23 00:27:06 +0530

    feat: refresh access token

--- Backend/Project-camp/src/controllers/refreshToken.controller.js
+++ Backend/Project-camp/src/controllers/refreshToken.controller.js
@@ -2,9 +2,52 @@ import { User } from '../models/user.models.js'
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { generateAccessAndRefreshToken } from './register.controller.js';
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
-    req.cookies
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized access")
+    }
+
+    try {
+        const decodedRefreshToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+        const user = await User.findById(decodedRefreshToken?._id)
+
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token")
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401, "Expired refresh token")
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        const {accessToken, refreshToken: newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+
+        user.refreshToken = newRefreshToken
+        await user.save()
+
+        return res
+                .status(200)
+                .cookie("accessToken", accessToken, options )
+                .cookie("refreshToken", newRefreshToken, options )
+                .json(
+                    new ApiResponse(
+                        200,
+                        {accessToken, refreshToken}
+                    )
+                )
+    } catch (error) {
+
+    }
 })
 
-export {refreshAccessToken}
+export { refreshAccessToken }
