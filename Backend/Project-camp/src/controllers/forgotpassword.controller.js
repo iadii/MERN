86b6a820fdea11f924ca680978e9b5eb266a5466
iadii <mishraaditya3456@gmail.com>
2026-02-23 22:44:07 +0530

    feat: forgot password

--- Backend/Project-camp/src/controllers/forgotpassword.controller.js
+++ Backend/Project-camp/src/controllers/forgotpassword.controller.js
@@ -3,9 +3,31 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { generateAccessAndRefreshToken } from './register.controller.js';
+import { forgotPasswordMailContent } from '../utils/mail.js';
 
 const forgotPasswordRequest = asyncHandler(async (req, res) => {
-    
+    const { email } = req.body;
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+    const { unHashedTokens, hashedTokens, TokenExpiry } = user.generateTemporaryToken()
+    user.forgotPasswordToken = hashedTokens
+    user.forgotPasswordExpiry = TokenExpiry
+
+    await user.save({ validateBeforeSave: false })
+
+    await sendEMail({
+        email: user?.email,
+        subject: "password reset request",
+        mailgenContent: forgotPasswordMailContent(
+            user?.username,
+            // `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedTokens}`
+            `${process.env.BASE_URL}/api/v1/users/verify-email/${unHashedTokens}`
+        )
+    })
 })
 
 export { forgotPasswordRequest }
