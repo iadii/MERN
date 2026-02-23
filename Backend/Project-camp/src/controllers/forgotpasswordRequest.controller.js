cf21096ad1a3749890205806750a7929db3fbe8b
iadii <mishraaditya3456@gmail.com>
2026-02-24 00:12:01 +0530

    feat: reset password

--- Backend/Project-camp/src/controllers/forgotpasswordRequest.controller.js
+++ Backend/Project-camp/src/controllers/forgotpasswordRequest.controller.js
@@ -2,7 +2,6 @@ import { User } from '../models/user.models.js'
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { generateAccessAndRefreshToken } from './register.controller.js';
 import { forgotPasswordMailContent } from '../utils/mail.js';
 
 const forgotPasswordRequest = asyncHandler(async (req, res) => {
---
+++ Backend/Project-camp/src/controllers/resetPassword.controller.js
@@ -0,0 +1,41 @@
+import { User } from '../models/user.models.js'
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { forgotPasswordMailContent } from '../utils/mail.js';
+
+const resetForgotPassword = asyncHandler(async (req, res) => {
+    const { resetToken } = req.params
+    const { newPassword } = req.body
+
+    let hashedToken = crypto
+        .createHash("256")
+        .update(resetToken)
+        .digest("hex")
+
+    const user = await User.findOne({
+        forgotPasswordToken: hashedToken,
+        forgotPasswordExpiry: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        throw new ApiError(
+            489, "Token is invalid or expired"
+        )
+    }
+
+    user.forgotPasswordExpiry = undefined
+    user.forgotPasswordToken = undefined
+
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
+
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, {}, "Password reset successfully")
+        )
+})
+
+export { resetForgotPassword }
