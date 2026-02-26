2532b97719c86679d30e1d86b36dbe0051b0a83f
iadii <mishraaditya3456@gmail.com>
2026-02-27 02:02:59 +0530

    feat: task schema

--- Backend/Project-camp/src/models/task.models.js
+++ Backend/Project-camp/src/models/task.models.js
@@ -1,5 +1,38 @@
 import mongoose, { Schema } from "mongoose";
-
+import { AvailableTaskStatus, TaskStatusEnum } from "../utils/constants";
 const TaskSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: String,
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    assignedTo: {
+         type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    assignedBy: {
+         type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    status: {
+        type: String,
+        enum: AvailableTaskStatus,
+        default: TaskStatusEnum.TODO
+    },
+    attachments: {
+        type: [{
+            url: String,
+            mimeType: Srting,
+            size: Number
+        }],
+        default: []
+    }
 }, {timestamps: true})
+
+export const Task = mongoose.model("Task", TaskSchema)
