bf852a8b96b03e24e73903ab5e23b67844ef6fe9
iadii <mishraaditya3456@gmail.com>
2026-02-27 17:25:34 +0530

    notes and subtask model

--- Backend/Project-camp/src/models/subtask.models.js
+++ Backend/Project-camp/src/models/subtask.models.js
@@ -1,5 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 
-const SubtaskSchema = new Schema({
-     
-}, {timestamps: true})
+const subTaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: ture
+    },
+    task: {
+        type: Schema.Types.ObjectId,
+        ref: "Task",
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, {timestamps: true}) 
+
+export const SubTask = mongoose.model("SubTask", subTaskSchema)
